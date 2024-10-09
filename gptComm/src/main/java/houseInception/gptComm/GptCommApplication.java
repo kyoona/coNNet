2a84d1a1402967282334ef2fce0d7f90a16524a3
kyoona <yoona405@konkuk.ac.kr>
2024-10-09 22:46:13 +0900

    spring initializer

---
+++ gptComm/src/main/java/houseInception/gptComm/GptCommApplication.java
@@ -0,0 +1,13 @@
+package houseInception.gptComm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptCommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptCommApplication.class, args);
+	}
+
+}
