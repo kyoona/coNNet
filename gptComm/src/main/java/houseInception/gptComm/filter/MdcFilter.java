add3d99bf8e93b74c13b1b101461956cbbb0524b
kyoona <yoona405@konkuk.ac.kr>
2024-10-13 11:58:26 +0900

    feat : MDC를 통해 스레드에 requeestId부여

---
+++ gptComm/src/main/java/houseInception/gptComm/filter/MdcFilter.java
@@ -0,0 +1,33 @@
+package houseInception.gptComm.filter;
+
+import jakarta.servlet.*;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class MdcFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        try {
+            String requestId = UUID.randomUUID().toString();
+            MDC.put("request_id", requestId);
+
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            MDC.clear();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
