e20242de6af01b9fb58d4068cb3f7dc59c908c50
kyoona <yoona405@konkuk.ac.kr>
2024-10-13 12:45:59 +0900

    feat : response dto에 requestId 추가

--- gptComm/src/main/java/houseInception/gptComm/response/BaseErrorResponse.java
+++ gptComm/src/main/java/houseInception/gptComm/response/BaseErrorResponse.java
@@ -4,6 +4,7 @@ import houseInception.gptComm.response.status.StatusCode;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.slf4j.MDC;
 import org.springframework.http.ResponseEntity;
 
 @Getter
@@ -16,10 +17,11 @@ public class BaseErrorResponse {
     public BaseErrorResponse(StatusCode code) {
         this.code = code.getCode();
         this.message = code.getMessage();
+        this.requestId = MDC.get("request_id");
     }
 
     public static ResponseEntity<BaseErrorResponse> get(StatusCode code) {
-        BaseErrorResponse res = new BaseErrorResponse(code.getCode(), code.getMessage(), null);
+        BaseErrorResponse res = new BaseErrorResponse(code.getCode(), code.getMessage(), MDC.get("request_id"));
         return new ResponseEntity<>(res, code.getHttpStatus());
     }
 }
--- gptComm/src/main/java/houseInception/gptComm/response/BaseResponse.java
+++ gptComm/src/main/java/houseInception/gptComm/response/BaseResponse.java
@@ -3,6 +3,7 @@ package houseInception.gptComm.response;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.slf4j.MDC;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
@@ -10,12 +11,11 @@ public class BaseResponse<T> {
     private String requestId;
     private T result;
 
-    //TODO 후에 MDC 스레드 UUID를 통해 requestId 변경
     public BaseResponse(T result) {
         this.result = result;
     }
 
     public static BaseResponse<BaseResultDto> getSimpleRes(Long id) {
-        return new BaseResponse<>(null, BaseResultDto.get(id));
+        return new BaseResponse<>(MDC.get("request_id"), BaseResultDto.get(id));
     }
 }
