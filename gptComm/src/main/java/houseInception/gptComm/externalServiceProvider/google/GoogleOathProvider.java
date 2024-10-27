3c78afb94a77a9c34a782d0d02f8d992f7484346
kyoona <yoona405@konkuk.ac.kr>
2024-10-27 20:05:11 +0900

    feat : access token 요청 로직 제거,id token 검증로직만 실행

--- gptComm/src/main/java/houseInception/gptComm/externalServiceProvider/google/GoogleOathProvider.java
+++ gptComm/src/main/java/houseInception/gptComm/externalServiceProvider/google/GoogleOathProvider.java
@@ -33,18 +33,14 @@ public class GoogleOathProvider {
     private String clientSecret;
 
     public GoogleUserInfo getUserInfo(String googleToken){
-        String accessToken = getAccessToken(googleToken);
-
-        String googleUserInfoEndpoint = "https://www.googleapis.com/oauth2/v2/userinfo";
+        String googleUserInfoEndpoint = "https://oauth2.googleapis.com/tokeninfo?id_token=" + googleToken;
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(accessToken);
-
         HttpEntity<String> entity = new HttpEntity<>(headers);
         try {
             ResponseEntity<GoogleUserInfo> response = restTemplate.exchange(
                     googleUserInfoEndpoint,
-                    HttpMethod.GET,
+                    HttpMethod.POST,
                     entity,
                     GoogleUserInfo.class
             );
@@ -54,45 +50,4 @@ public class GoogleOathProvider {
             throw new InValidTokenException(INVALID_GOOGLE_TOKEN);
         }
     }
-
-    private String getAccessToken(String googleToken) {
-        String googleAccessKeyEndpoint = "https://oauth2.googleapis.com/token";
-        log.info("googleToken = {}", googleToken);
-
-        Map<String, String> requestBodyMap = new HashMap<>();
-        requestBodyMap.put("code", googleToken);
-        requestBodyMap.put("client_id", clientId);
-        requestBodyMap.put("client_secret", clientSecret);
-        requestBodyMap.put("redirect_uri", "http://localhost:5173/login");
-        requestBodyMap.put("grant_type", "authorization_code");
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        String requestBody;
-        try {
-            requestBody = objectMapper.writeValueAsString(requestBodyMap);
-        } catch (JsonProcessingException e) {
-            throw new JsonParseException();
-        }
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(org.springframework.http.MediaType.APPLICATION_JSON);
-
-        HttpEntity<String> accessTokenRequestEntity = new HttpEntity<>(requestBody, headers);
-
-        ResponseEntity<GoogleAccessToken> response;
-        try {
-            response = restTemplate.exchange(
-                    googleAccessKeyEndpoint,
-                    HttpMethod.POST,
-                    accessTokenRequestEntity,
-                    GoogleAccessToken.class
-            );
-        } catch (Exception e) {
-            log.error("Error while requesting access token", e);
-            throw new InValidTokenException(INVALID_GOOGLE_TOKEN);
-        }
-
-        return response.getBody().getAccess_token();
-    }
-
 }
