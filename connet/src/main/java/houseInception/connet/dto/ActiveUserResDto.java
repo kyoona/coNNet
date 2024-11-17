db04b33e5bf2936b1d397aa8fb249413afe4e84b
kyoona <yoona405@konkuk.ac.kr>
2024-11-17 20:13:01 +0900

    feat : 친구 목록 조회에 유저 활성화 유무 필드 추가

--- connet/src/main/java/houseInception/connet/dto/ActiveUserResDto.java
+++ connet/src/main/java/houseInception/connet/dto/ActiveUserResDto.java
@@ -11,11 +11,13 @@ public class ActiveUserResDto {
     private Long userId;
     private String userName;
     private String userProfile;
+    private boolean isActive;
 
     @QueryProjection
-    public ActiveUserResDto(Long userId, String userName, String userProfile) {
+    public ActiveUserResDto(Long userId, String userName, String userProfile, boolean isActive) {
         this.userId = userId;
         this.userName = userName;
         this.userProfile = userProfile;
+        this.isActive = isActive;
     }
 }
