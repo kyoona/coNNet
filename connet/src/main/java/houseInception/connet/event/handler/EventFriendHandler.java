a4f52464c87a02dfca863efcf7ab1537d228ff3c
kyoona <yoona405@konkuk.ac.kr>
2024-11-20 23:52:58 +0900

    refactor : FriendService의 Exeption을 throw하는 코드 리팩토링

--- connet/src/main/java/houseInception/connet/event/handler/EventFriendHandler.java
+++ connet/src/main/java/houseInception/connet/event/handler/EventFriendHandler.java
@@ -3,19 +3,23 @@ package houseInception.connet.event.handler;
 import houseInception.connet.event.domain.UserBlockEvent;
 import houseInception.connet.service.FriendService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
-public class UserBlockEventHandler {
+public class EventFriendHandler {
 
     private final FriendService friendService;
 
     @Async
-    @TransactionalEventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void deleteFriend(UserBlockEvent event){
+        log.info("이벤트핸들러!");
         friendService.deleteFriend(event.getUserId(), event.getTargetId());
     }
 }
