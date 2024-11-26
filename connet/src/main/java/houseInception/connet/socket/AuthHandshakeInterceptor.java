10c4ca42f0ecf808fc5032c783f69034e1893881
kyoona <yoona405@konkuk.ac.kr>
2024-11-26 13:05:02 +0900

    feat : 소켓 연결시 토큰을 헤더로 받았던 것을 쿼리 파라미터로 수정

--- connet/src/main/java/houseInception/connet/socket/AuthHandshakeInterceptor.java
+++ connet/src/main/java/houseInception/connet/socket/AuthHandshakeInterceptor.java
@@ -5,14 +5,16 @@ import houseInception.connet.domain.User;
 import houseInception.connet.jwt.JwtTokenProvider;
 import houseInception.connet.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.MDC;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -24,17 +26,32 @@ public class AuthHandshakeInterceptor implements HandshakeInterceptor {
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-        String token = getToken(request.getHeaders());
+        String token = getToken(request.getURI());
+        String userEmail = checkValidToken(token);
 
-        String userEmail = tokenProvider.getUserPk(token);
+        if (userEmail == null) {
+            return false;
+        }
+
         User user = userRepository.findByEmailAndStatus(userEmail, Status.ALIVE).orElseThrow();
-
         attributes.put("Socket-User-Id", user.getId());
+
         return true;
     }
 
-    private String getToken(HttpHeaders headers){
-        return headers.getFirst("Authorization").substring(7);
+    private String  checkValidToken(String token){
+        if(token != null && tokenProvider.validateToken(token)){
+            return tokenProvider.getUserPk(token);
+        }
+
+        return null;
+    }
+
+    private String getToken(URI uri){
+        return UriComponentsBuilder.fromUri(uri)
+                .build()
+                .getQueryParams()
+                .getFirst("token");
     }
 
     @Override
